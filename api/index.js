3d46aac969a1d452f1ed7b9cee70761560602caf
SantiCombina <santicombina@gmail.com>
2024-08-15 16:19:09 -0300

    Update API route configuration and file paths for vercel deployment

--- api/index.js
+++ api/index.js
@@ -1,17 +1,3 @@
-import cors from "cors";
-import express from "express";
-import salesRoutes from "../routes/sales.routes.js";
-import budgetsRoutes from "../routes/budgets.routes.js";
-import configRoutes from "../routes/config.routes.js";
+import app from "../api/index.js";
 
-const app = express();
-
-app.use(express.json());
-app.use(cors());
-
-app.use("/api", salesRoutes);
-app.use("/api", budgetsRoutes);
-app.use("/api", configRoutes);
-
-app.listen(3000);
-console.log("server started on port 3000");
+export default app;
