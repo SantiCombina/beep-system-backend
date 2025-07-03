6a6e39d34d4446583787bca474f117c2ea554355
SantiCombina <santicombina@gmail.com>
2025-07-03 18:45:27 -0300

    Refactor CORS configuration to use default settings

--- src/index.js
+++ src/index.js
@@ -7,16 +7,7 @@ import configRoutes from "./routes/config.routes.js";
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-  origin: [
-    'https://beep-system.vercel.app',
-    'http://localhost:3000',
-    'http://localhost:3001'
-  ],
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors());
 
 app.use("/api", salesRoutes);
 app.use("/api", budgetsRoutes);
